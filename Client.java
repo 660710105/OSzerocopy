dbdd4b5357793b76b83ec56694a0f18cf56ec200
660710099 <saree_w@silpakorn.edu>
2025-10-30 13:26:41 +0700

    formatting code

--- Client.java
+++ Client.java
@@ -1,107 +1,112 @@
 import java.io.*;
 import java.net.*;
-import java.nio.file.Files;
-import java.nio.file.Path;
-import java.nio.file.Paths;
 import java.util.List;
 import java.util.Scanner;
 
-class Client implements Runnable{
+class Client implements Runnable {
+        private String host;
+        private int port;
+        private File targetDir;
 
-    private String host;
-    private int port;
-    private File targetDir;
-
-    public Client(String host, int port, String pathFile){
-        this.host = (host.isEmpty())? 
-            "localhost":
-             host;
-
-        this.port = port;
-
-        this.targetDir = (pathFile.isEmpty())?
-            new File("./mailBox"):
-            new File(pathFile);
-        
-        if (!targetDir.exists())
-            targetDir.mkdirs();
-    }
-
-    @Override
-    public void run() {
-        System.out.println(" >> Client download at " + targetDir);
-        
-        try {
-            @SuppressWarnings("resource")
-            Socket socket = new Socket(host, port);
-	    socket.setKeepAlive(true);
-            ObjectInputStream oin = new ObjectInputStream(socket.getInputStream());
-            ObjectOutputStream oout = new ObjectOutputStream(socket.getOutputStream());
-            System.out.println(" >> Connected Server " + host + ": " + port);
-
-            Object obj = oin.readObject();
-            if (!(obj instanceof List)) {
-                System.err.println("Error: not list");
-                return;
-            }
-            @SuppressWarnings("unchecked")
-            List<String> listFilenames = (List<String>) obj;
-            System.out.println("=== List files ===");
-            for (int i=0 ; i < listFilenames.size(); i++)
-                System.out.println( i + ":  " + listFilenames.get(i));
-
-            Scanner sc = new Scanner(System.in);
-            System.out.println("=== Select File ===");
-            System.out.print("Enter index of file: ");
-            int indexFile = Integer.parseInt(sc.nextLine());
-            String filename = listFilenames.get(indexFile);
-
-            System.out.println("=== Select Mode === \n 0 = Copy \n 1 = Zero-copy \n 2 = Buffered");
-            System.out.print("Select: ");
-            String mode = sc.nextLine();
-
-            oout.writeObject(filename);
-            oout.writeObject(mode);
-            oout.flush();
-
-            boolean exists = oin.readBoolean();
-            System.out.println(exists);
-
-            // long fileSize = oin.readLong();
-            // System.out.println("=== Server will send " + fileSize + " bytes. ===");
-
-            File outFile = new File(targetDir, filename);
-            FileOutputStream fos = new FileOutputStream(outFile);
-            long start = System.currentTimeMillis();
-
-            byte[] fileContent = (byte[]) oin.readObject();
-            fos.write(fileContent);
-            System.out.println("self destruction");
-
-            // InputStream in = socket.getInputStream();
-            
-            // try {
-            //     byte[] buffer = new byte[1024 * 1024];
-            //     long remain = fileSize;
-            //     while (remain > 0) {
-            //         int read = (buffer.length > remain) ? (int) buffer.length : (int) remain;
-            //         int r = in.read(buffer, 0, read);
-            //         fos.write(buffer, 0, r);
-            //         remain -= r;
-            //     }
-            //     fos.flush();
-            // } finally {
-            //     long end = System.currentTimeMillis();
-            //     System.out.printf("Downloaded to %s (%d ms)\n", outFile.getAbsolutePath(), (end - start));
-            //     fos.close();
-            // }
-        } catch (SocketException s) {
-            System.out.println("Disconnected server");
-            s.printStackTrace();
-        } catch (IOException i){
-            i.printStackTrace();
-        } catch (ClassNotFoundException c) {
-            c.printStackTrace();
+        public Client(String host, int port, String pathFile){
+                this.host = host;
+                this.port = port;
+                this.targetDir = new File(pathFile);
+                
+                if (!targetDir.exists()) {
+                        targetDir.mkdirs();
+                }
         }
-    }
+
+        @Override
+        public void run() {
+                Scanner sc = new Scanner(System.in);
+                Socket socket;
+                ObjectInputStream oin;
+                ObjectOutputStream oout;
+                
+                try {
+                        System.out.println(" >> Client download at " + targetDir);
+
+                        socket = new Socket(host, port);
+                        socket.setKeepAlive(true);
+                        
+                        System.out.println(" >> Connected Server " + host + ": " + port);
+
+                        oin = new ObjectInputStream(socket.getInputStream());
+                        oout = new ObjectOutputStream(socket.getOutputStream());
+
+                        Object obj = oin.read();
+
+                        @SuppressWarnings("unchecked") List<String> listFilenames = (List<String>)obj;
+                        
+                        System.out.println("=== List files ===");
+                        
+                        for (int i=0 ; i < listFilenames.size(); i++) {
+                                System.out.println( i + ":  " + listFilenames.get(i));
+                        }
+                        
+                        System.out.println("=== Select File ===");
+                        System.out.print("Enter index of file: ");
+                        int indexFile = Integer.parseInt(sc.nextLine());
+                        String filename = listFilenames.get(indexFile);
+
+                        System.out.println("=== Select Mode === \n 0 = Copy \n 1 = Zero-copy \n 2 = Buffered");
+                        System.out.print("Select: ");
+                        String mode = sc.nextLine();
+
+                        oout.writeObject(filename);
+                        oout.writeObject(mode);
+                        oout.flush();
+
+                        boolean exists = oin.readBoolean();
+                        System.out.println(exists);
+
+                        // long fileSize = oin.readLong();
+                        // System.out.println("=== Server will send " + fileSize + " bytes. ===");
+
+                        File outFile = new File(targetDir, filename);
+                        FileOutputStream fos = new FileOutputStream(outFile);
+                        long start = System.currentTimeMillis();
+
+                        byte[] fileContent = (byte[]) oin.readObject();
+                        fos.write(fileContent);
+                        
+                        fos.close();
+                        
+                        System.out.println("self destruction");
+
+                        // InputStream in = socket.getInputStream();
+            
+                        // try {
+                        //     byte[] buffer = new byte[1024 * 1024];
+                        //     long remain = fileSize;
+                        //     while (remain > 0) {
+                        //         int read = (buffer.length > remain) ? (int) buffer.length : (int) remain;
+                        //         int r = in.read(buffer, 0, read);
+                        //         fos.write(buffer, 0, r);
+                        //         remain -= r;
+                        //     }
+                        //     fos.flush();
+                        // } finally {
+                        //     long end = System.currentTimeMillis();
+                        //     System.out.printf("Downloaded to %s (%d ms)\n", outFile.getAbsolutePath(), (end - start));
+                        //     fos.close();
+                        // }
+
+                        // closing this will throw exception, so it needed to be is this block.
+                        oin.close();
+                        oout.close();
+                        socket.close();
+                } catch (SocketException s) {
+                        System.out.println("Disconnected server");
+                        s.printStackTrace();
+                } catch (IOException i){
+                        i.printStackTrace();
+                } catch (ClassNotFoundException c) {
+                        c.printStackTrace();
+                } finally {
+                        sc.close();
+                }
+        }
 }
--- Main.java
+++ Main.java
@@ -1,90 +1,90 @@
 import java.util.Scanner;
 
 public class Main {
-	static int port = 9090;
-	static String directory = "./mailbox";
-	static String sendDirectory = "./send";
-	static String remoteHost = "localhost";
-	static boolean runServer = false;
+        static int port = 9090;
+        static String directory = "./mailbox";
+        static String sendDirectory = "./send";
+        static String remoteHost = "localhost";
+        static boolean runServer = false;
 
-	public static void printUsages() {
-		System.out.print("examples: \n" +
-				 " <app_name> [ -s {-d, -D} <directory> -p <port> -h ]\n" +
-				 "usages: \n" +
-				 " --server, -s         run this as a server.\n" +
-				 " --directory, -d      set mailbox directory.\n" +
-				 " --send-directory, -D set send directory.\n" +
-				 " --port, -p           set port to connect or listening.\n" +
-				 " --host, -h           set remote host.\n"
-				   );
-	}
-	
-	public static void main(String[] args) {
-		Scanner sc = new Scanner(System.in);
-		int argsIdx = 0;
-		while(argsIdx < args.length) {
-			switch (args[argsIdx]) {
-			case "--port":
-			case "-p":
-				if (argsIdx + 1 >= args.length) {
-					printUsages();
-					return;
-				}
-				port = Integer.parseInt(args[argsIdx + 1]);
-				argsIdx += 2;
-				break;
-			case "--directory":
-			case "-d":
-				if (argsIdx + 1 >= args.length) {
-					printUsages();
-					return;
-				}
-				directory = args[argsIdx + 1];
-				argsIdx += 2;
-				break;
-			case "--send-directory":
-			case "-D":
-				if (argsIdx + 1 >= args.length) {
-					printUsages();
-					return;
-				}
-				sendDirectory = args[argsIdx + 1];
-				argsIdx += 2;
-				break;
-			case "--host":
-			case "-h":
-				if (argsIdx + 1 >= args.length) {
-					printUsages();
-					return;
-				}
-				remoteHost = args[argsIdx + 1];
-				argsIdx += 2;
-				break;
-			case "--server":
-			case "-s":
-				runServer = true;
-				argsIdx++;
-				break;
-			default:
-				printUsages();
-				return;
-			} 
-		}
+        public static void printUsages() {
+                System.out.print("examples: \n" +
+                                 " <app_name> [ -s {-d, -D} <directory> -p <port> -h ]\n" +
+                                 "usages: \n" +
+                                 " --server, -s         run this as a server.\n" +
+                                 " --directory, -d      set mailbox directory.\n" +
+                                 " --send-directory, -D set send directory.\n" +
+                                 " --port, -p           set port to connect or listening.\n" +
+                                 " --host, -h           set remote host.\n"
+                                 );
+        }
+        
+        public static void main(String[] args) {
+                Scanner sc = new Scanner(System.in);
+                int argsIdx = 0;
+                while(argsIdx < args.length) {
+                        switch (args[argsIdx]) {
+                        case "--port":
+                        case "-p":
+                                if (argsIdx + 1 >= args.length) {
+                                        printUsages();
+                                        return;
+                                }
+                                port = Integer.parseInt(args[argsIdx + 1]);
+                                argsIdx += 2;
+                                break;
+                        case "--directory":
+                        case "-d":
+                                if (argsIdx + 1 >= args.length) {
+                                        printUsages();
+                                        return;
+                                }
+                                directory = args[argsIdx + 1];
+                                argsIdx += 2;
+                                break;
+                        case "--send-directory":
+                        case "-D":
+                                if (argsIdx + 1 >= args.length) {
+                                        printUsages();
+                                        return;
+                                }
+                                sendDirectory = args[argsIdx + 1];
+                                argsIdx += 2;
+                                break;
+                        case "--host":
+                        case "-h":
+                                if (argsIdx + 1 >= args.length) {
+                                        printUsages();
+                                        return;
+                                }
+                                remoteHost = args[argsIdx + 1];
+                                argsIdx += 2;
+                                break;
+                        case "--server":
+                        case "-s":
+                                runServer = true;
+                                argsIdx++;
+                                break;
+                        default:
+                                printUsages();
+                                return;
+                        } 
+                }
 
-		System.out.println("server: " + runServer + ", host: " + remoteHost + ", port: " + port + ", dir: " + directory + ", send dir: " + sendDirectory);
-		
-		try {
-			if (runServer) {
-				Server server = new Server(sendDirectory, port);
-				server.run();
-			} else {
-				Client client = new Client(remoteHost, port, directory);
-				client.run();
-			}
-		} catch (Exception e) {
-			
-		}
-		
-		sc.close();
-	}
+                System.out.println("server: " + runServer + ", host: " + remoteHost + ", port: " + port + ", dir: " + directory + ", send dir: " + sendDirectory);
+                
+                try {
+                        if (runServer) {
+                                Server server = new Server(sendDirectory, port);
+                                server.run();
+                        } else {
+                                Client client = new Client(remoteHost, port, directory);
+                                client.run();
+                        }
+                } catch (Exception e) {
+                        
+                }
+                
+                sc.close();
+        }
 }
