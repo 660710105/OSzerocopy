35a4b87bff7074e21e52dbe87d6afc805b39810f
660710099 <saree_w@silpakorn.edu>
2025-11-09 21:48:48 +0700

    main: printing more useful infomation.

--- zerocopy/Main.java
+++ zerocopy/Main.java
@@ -1,94 +1,122 @@
 package zerocopy;
 
-import java.util.Scanner;
+import java.net.InetAddress;
+import java.net.NetworkInterface;
+import java.net.SocketException;
+import java.net.UnknownHostException;
+import java.util.Enumeration;
+import java.util.Iterator;
+
 import zerocopy.client.*;
 import zerocopy.server.*;
 
 public class Main {
-        static int port = 9090;
-        static String directory = "./mailbox";
-        static String sendDirectory = "./send";
-        static String remoteHost = "localhost";
-        static boolean runServer = false;
+    static int port = 9090;
+    static String directory = "./mailbox";
+    static String sendDirectory = "./send";
+    static String remoteHost = "localhost";
+    static boolean runServer = false;
 
-        public static void printUsages() {
-                System.out.print("examples: \n" +
-                                 " <app_name> [ -s {-d, -D} <directory> -p <port> -h ]\n" +
-                                 "usages: \n" +
-                                 " --server, -s         run this as a server.\n" +
-                                 " --directory, -d      set mailbox directory.\n" +
-                                 " --send-directory, -D set send directory.\n" +
-                                 " --port, -p           set port to connect or listening.\n" +
-                                 " --host, -h           set remote host.\n"
-                                 );
-        }
+    public static void printUsages() {
+        System.out.print("examples: \n" +
+                         " <app_name> [ -s ] [ { -d, -D } <directory> ] [ -p <port>] [ -h ] ]\n" +
+                         "usages: \n" +
+                         " --server, -s         run this as a server.\n" +
+                         " --directory, -d      set mailbox directory.\n" +
+                         " --send-directory, -D set send directory.\n" +
+                         " --port, -p           set port to connect or listening.\n" +
+                         " --host, -h           set remote host.\n"
+                         );
+    }
         
-        public static void main(String[] args) {
-                Scanner sc = new Scanner(System.in);
-                int argsIdx = 0;
-                while(argsIdx < args.length) {
-                        switch (args[argsIdx]) {
-                        case "--port":
-                        case "-p":
-                                if (argsIdx + 1 >= args.length) {
-                                        printUsages();
-                                        return;
-                                }
-                                port = Integer.parseInt(args[argsIdx + 1]);
-                                argsIdx += 2;
-                                break;
-                        case "--directory":
-                        case "-d":
-                                if (argsIdx + 1 >= args.length) {
-                                        printUsages();
-                                        return;
-                                }
-                                directory = args[argsIdx + 1];
-                                argsIdx += 2;
-                                break;
-                        case "--send-directory":
-                        case "-D":
-                                if (argsIdx + 1 >= args.length) {
-                                        printUsages();
-                                        return;
-                                }
-                                sendDirectory = args[argsIdx + 1];
-                                argsIdx += 2;
-                                break;
-                        case "--host":
-                        case "-h":
-                                if (argsIdx + 1 >= args.length) {
-                                        printUsages();
-                                        return;
-                                }
-                                remoteHost = args[argsIdx + 1];
-                                argsIdx += 2;
-                                break;
-                        case "--server":
-                        case "-s":
-                                runServer = true;
-                                argsIdx++;
-                                break;
-                        default:
-                                printUsages();
-                                return;
-                        } 
+    public static void main(String[] args) {
+        int argsIdx = 0;
+        while(argsIdx < args.length) {
+            switch (args[argsIdx]) {
+            case "--port":
+            case "-p":
+                if (argsIdx + 1 >= args.length) {
+                    printUsages();
+                    return;
+                }
+                port = Integer.parseInt(args[argsIdx + 1]);
+                argsIdx += 2;
+                break;
+            case "--directory":
+            case "-d":
+                if (argsIdx + 1 >= args.length) {
+                    printUsages();
+                    return;
+                }
+                directory = args[argsIdx + 1];
+                argsIdx += 2;
+                break;
+            case "--send-directory":
+            case "-D":
+                if (argsIdx + 1 >= args.length) {
+                    printUsages();
+                    return;
+                }
+                sendDirectory = args[argsIdx + 1];
+                argsIdx += 2;
+                break;
+            case "--host":
+            case "-h":
+                if (argsIdx + 1 >= args.length) {
+                    printUsages();
+                    return;
+                }
+                remoteHost = args[argsIdx + 1];
+                argsIdx += 2;
+                break;
+            case "--server":
+            case "-s":
+                runServer = true;
+                argsIdx++;
+                break;
+            default:
+                printUsages();
+                return;
+            } 
+        }
+
+        StringBuilder infoStringBuilder = new StringBuilder();
+
+        try {
+            if (runServer) {
+                infoStringBuilder
+                    .append("Select hosting directory: " + directory + "\n")
+                    .append("Now Server will listening is this following interfaces: \n");
+                
+                Enumeration<NetworkInterface> interfaceLookup = NetworkInterface.getNetworkInterfaces();
+                while (interfaceLookup.hasMoreElements()) {
+                    NetworkInterface networkInterface = interfaceLookup.nextElement();
+                    String networkInterfaceName = networkInterface.getDisplayName();
+                    Enumeration<InetAddress> addressLookup = networkInterface.getInetAddresses();
+                    while (addressLookup.hasMoreElements()) {
+                        InetAddress address = addressLookup.nextElement();
+                        infoStringBuilder.append(networkInterfaceName + ": " + address.getHostAddress() + ":" + port + "\n");
+                    }
                 }
 
-                System.out.println("server: " + runServer + ", host: " + remoteHost + ", port: " + port + ", dir: " + directory + ", send dir: " + sendDirectory);
+                String info = infoStringBuilder.toString();
+                System.out.println(info);
                 
-                try {
-                        if (runServer) {
-                                Thread server = new Thread(new Server(sendDirectory, port));
-                                server.run();
-                        } else {
-                                Thread client = new Thread(new Client(remoteHost, port, directory));
-                                client.run();
-                        }
-                } catch (Exception e) {
-                        
-                }
+                Thread server = new Thread(new Server(sendDirectory, port));
+                server.run();
+            } else {
+                String info = infoStringBuilder
+                    .append("Selecting remote: " + remoteHost + " ")
+                    .append("on port " + port + "\n")
+                    .toString();
+
+                System.out.println(info);
                 
-                sc.close();
+                Thread client = new Thread(new Client(remoteHost, port, directory));
+                client.run();
+            }
+        } catch (SocketException exception) {
+            exception.printStackTrace();
         }
+    }
 }
