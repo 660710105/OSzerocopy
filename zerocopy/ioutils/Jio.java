c99ac19f68a69b5d839c9b017fd6bf0952e8618c
660710105 <charoenkitmongk_s@silpakorn.edu>
2025-11-11 00:46:44 +0700

    change name variable

--- zerocopy/ioutils/Jio.java
+++ zerocopy/ioutils/Jio.java
@@ -73,7 +73,7 @@ public class Jio {
         }
     }
 
-    public void multiThread(File file, long fileSize,String host, int port, int nthread, CopyMode mode, File targetDir) throws IOException, InterruptedException {
+    public void multiThread(File outFile, long fileSize,String host, int port, int nthread, CopyMode mode, File targetDir) throws IOException, InterruptedException {
         long partSize = fileSize / nthread;
         
         Thread[] threads = new Thread[nthread];
@@ -91,10 +91,10 @@ public class Jio {
                 endByte = startByte + partSize - 1;
             }
 
-            String tempPartName = file.getName() + ".part." + i;
+            String tempPartName = outFile.getName() + ".part." + i;
             partFiles[i] = new File(targetDir, tempPartName);
             
-            SendThread worker = new SendThread(i, file, host, port, startByte, endByte,partFiles[i].getAbsolutePath() , mode);
+            SendThread worker = new SendThread(i, outFile, host, port, startByte, endByte,partFiles[i].getAbsolutePath() , mode);
             threads[i] = new Thread(worker);
             threads[i].start();
         }
@@ -105,7 +105,7 @@ public class Jio {
 
         System.out.println("All threads finished writing.");
 
-        try (FileOutputStream fos = new FileOutputStream(file);
+        try (FileOutputStream fos = new FileOutputStream(outFile);
              FileChannel destChannel = fos.getChannel()) {
             
             for (int i = 0; i < nthread; i++) {
