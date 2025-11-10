06a70aae3c85f8cb89183ba8aa3a835aecbea5e0
660710105 <charoenkitmongk_s@silpakorn.edu>
2025-11-10 15:11:00 +0700

    fix .bat

--- spawn.bat
+++ spawn.bat
@@ -3,7 +3,7 @@ SETLOCAL
 
 REM 1. Find all .java files recursively under 'zerocopy'
 REM    and write their full paths to a temporary file.
-(for /R zerocopy %%F in (*.java) do echo "%%~fF") > __java_files.txt
+(for /R zerocopy %%F in (*.java) do echo %%~F) > __java_files.txt
 
 REM 2. Compile all files listed in the temp file using javac's @argfile feature.
 javac -d build @__java_files.txt
